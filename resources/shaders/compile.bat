c6344bfd0b288d0d66fbaa54aea35e749fdfdfd2
Lars L R <lars.lahlum.ruud@tietoevry.com>
2024-06-26 14:56:11 +0200

    Add debug/HL info argument to .spv compilation script. Remove old files.

--- resources/shaders/compile.bat
+++ resources/shaders/compile.bat
@@ -1,4 +1,4 @@
-C:/VulkanSDK/1.3.275.0/Bin/glslc.exe shader.vert --target-env=vulkan1.3 -o vert.spv
-C:/VulkanSDK/1.3.275.0/Bin/glslc.exe shader.frag --target-env=vulkan1.3 -o frag.spv
-C:/VulkanSDK/1.3.275.0/Bin/glslc.exe shader.comp --target-env=vulkan1.3 -o comp.spv
+C:/VulkanSDK/1.3.275.0/Bin/glslc.exe shader.vert --target-env=vulkan1.3 -o vert.spv -g
+C:/VulkanSDK/1.3.275.0/Bin/glslc.exe shader.frag --target-env=vulkan1.3 -o frag.spv -g
+C:/VulkanSDK/1.3.275.0/Bin/glslc.exe shader.comp --target-env=vulkan1.3 -o comp.spv -g
 pause
